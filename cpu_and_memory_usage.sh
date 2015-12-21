1fec867ed12322526a48df0eb967ec48e8d69287
Sinesio Bittencourt <contato@sinesio.com.br>
2015-12-20 22:01:27 -0200

    up

---
+++ cpu_and_memory_usage.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+##################################################################
+# Author       : Sinesio Bittencourt
+# Email        : contato@sinesio.com.br
+# Info         : Script to check cPanel CPU and Memory usage for a specific user
+# Project      : http://git.hostgator.com.br
+
+##################################################################
+VERSAO="0.1b"
+
+clear
+
+printf "
+# Author       : Sinesio Bittencourt
+# Email        : contato@sinesio.com.br
+# Version      : v$VERSAO ( beta )
+"
+echo -e "\n[!] - Bem vindo.\n"
+
+read -p "Por favor informe o dominio a ser analisado: "		DOMAIN
+
+#domain="xxx.xx.xx";
+
+for i in `seq 1 7 `;
+    do let i=$i+1 ; let  k=$i-1 ; let s="$(date +%s) - (k-1)*86400"; let t="$(date +%s) - (k-2)*86400";
+    echo `date -Idate -d @$s`; /usr/local/cpanel/bin/dcpumonview `date -d @$s +%s` `date -d @$t +%s` |
+    sed -r -e 's@^<tr bgcolor=#[[:xdigit:]]+><td>(.*)</td><td>(.*)</td><td>(.*)</td><td>(.*)</td><td>(.*)</td></tr>$@Account: \1\tDomain: \2\tCPU: \3\tMem: \4\tMySQL: \5@' -e 's@^<tr><td>Top Process</td><td>(.*)</td><td colspan=3>(.*)</td></tr>$@\1 - \2@' |
+    grep $domain -A3 ;
+done
+
