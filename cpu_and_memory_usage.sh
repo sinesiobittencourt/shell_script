d22adffab4750501999be3646aa035376089f491
SinesioBittencourt <contato@sinesio.com.br>
2015-12-20 22:20:53 -0200

    up

--- cpu_and_memory_usage.sh
+++ cpu_and_memory_usage.sh
@@ -9,7 +9,7 @@
 ##################################################################
 VERSAO="0.1b"
 
-clear
+#clear
 
 printf "
 # Author       : Sinesio Bittencourt
@@ -18,14 +18,16 @@ printf "
 "
 echo -e "\n[!] - Bem vindo.\n"
 
-read -p "Por favor informe o dominio a ser analisado: "		DOMAIN
+read -p "Por favor informe o dominio a ser analisado: " DOMAIN
 
 #domain="xxx.xx.xx";
+#/usr/local/cpanel/bin/dcpumonview
 
-for i in `seq 1 7 `;
-    do let i=$i+1 ; let  k=$i-1 ; let s="$(date +%s) - (k-1)*86400"; let t="$(date +%s) - (k-2)*86400";
-    echo `date -Idate -d @$s`; /usr/local/cpanel/bin/dcpumonview `date -d @$s +%s` `date -d @$t +%s` |
-    sed -r -e 's@^<tr bgcolor=#[[:xdigit:]]+><td>(.*)</td><td>(.*)</td><td>(.*)</td><td>(.*)</td><td>(.*)</td></tr>$@Account: \1\tDomain: \2\tCPU: \3\tMem: \4\tMySQL: \5@' -e 's@^<tr><td>Top Process</td><td>(.*)</td><td colspan=3>(.*)</td></tr>$@\1 - \2@' |
-    grep $domain -A3 ;
+for i in `seq 1 7`;
+    do
+      let i=$i+1 ;
+      let  k=$i-1 ;
+      let s="$(date +%s) - (k-1)*86400";
+      let t="$(date +%s) - (k-2)*86400";
+      echo `date -Idate -d @$s`; /usr/local/cpanel/bin/dcpumonview `date -d @$s +%s` `date -d @$t +%s` | sed -r -e 's@^<tr bgcolor=#[[:xdigit:]]+><td>(.*)</td><td>(.*)</td><td>(.*)</td><td>(.*)</td><td>(.*)</td></tr>$@Account: \1\tDomain: \2\tCPU: \3\tMem: \4\tMySQL: \5@' -e 's@^<tr><td>Top Process</td><td>(.*)</td><td colspan=3>(.*)</td></tr>$@\1 - \2@' | grep $domain -A3 ;
 done
-
