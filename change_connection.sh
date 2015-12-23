2e48dc6f1b39a1fe120eaa98412f944a4bbb6d49
Sinesio Bittencourt <contato@sinesio.com.br>
2015-12-23 16:41:47 -0200

    CONECT_OLD_DB and CONECT_NEW_DB

---
+++ change_connection.sh
@@ -0,0 +1,3 @@
+# Mudar o caminho da conexão
+# Change the Connection
+for USER in `ls | oi USER_WONER | cut -d- -f1`; do cd /home/$USER/www/; grep "CONECT_OLD_DB" * -R | awk -F: '{print $1}' | xargs -n1 sed -i 's/CONECT_OLD_DB/CONECT_NEW_DB/g' $1;  done
