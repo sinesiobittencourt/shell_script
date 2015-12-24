cfa2ef8e7089c0d8e18203e863de957a7697efe0
Sinesio Bittencourt <contato@sinesio.com.br>
2015-12-24 11:05:46 -0200

    Update change_connection.sh

--- change_connection.sh
+++ change_connection.sh
@@ -1,3 +1,18 @@
+#!/bin/bash
+
+##################################################################
+# Author       : Sinesio Bittencourt
+# Email        : contato@sinesio.com.br
+# Info         : Change the Connection
+# Project      : http://git.sinesio.com.br or https://github.com/sinesiobittencourt/
+##################################################################
+VERSAO="0.1b"
+
+#clear
+
+printf "# Version      : v$VERSAO ( beta )"
+
+echo -e "\n[!] - Bem vindo.\n"
+
 # Mudar o caminho da conexão
-# Change the Connection
 for USER in `ls | oi USER_OWNER | cut -d- -f1`; do cd /home/$USER/www/; grep "CONECT_OLD_DB" * -R | awk -F: '{print $1}' | xargs -n1 sed -i 's/CONECT_OLD_DB/CONECT_NEW_DB/g' $1;  done
