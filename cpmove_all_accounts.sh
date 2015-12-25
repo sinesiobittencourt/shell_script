3c7dc2135380441651d07ab7a805ca0849a0b654
Sinesio Bittencourt <contato@sinesio.com.br>
2015-12-25 18:42:04 -0200

    Update cpmove_all_accounts.sh

--- cpmove_all_accounts.sh
+++ cpmove_all_accounts.sh
@@ -1,4 +1,12 @@
-# Generate all accounts cpmove in server #
+#!/bin/bash
+
+##################################################################
+# Author       : Sinesio Bittencourt
+# Email        : contato@sinesio.com.br
+# Info         : Generate all accounts cpmove in server (cPanel)
+# Project      : http://git.sinesio.com.br or https://github.com/sinesiobittencourt/
+##################################################################
+
 # GERAR CPMOVE de todas as contas em um Dedicado ou VPS #
 
 for S in $(cat /etc/trueuserdomains |cut -d: -f2 | awk '{print $1}');do /scripts/pkgacct --skip-limit $S ;done
