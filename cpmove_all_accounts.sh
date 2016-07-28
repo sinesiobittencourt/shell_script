8f89beb18ece377e803145a0ae39a4334dd69593
leonardoipx <leonardo.ipx@gmail.com>
2016-07-27 23:29:24 -0300

    Update cpmove_all_accounts.sh
    Comando não existe mais

--- cpmove_all_accounts.sh
+++ cpmove_all_accounts.sh
@@ -9,4 +9,4 @@
 
 # GERAR CPMOVE de todas as contas em um Dedicado ou VPS #
 
-for S in $(cat /etc/trueuserdomains |cut -d: -f2 | awk '{print $1}'); do /scripts/pkgacct --skip-limit $S ;done
+for S in $(cat /etc/trueuserdomains |cut -d: -f2 | awk '{print $1}'); do /scripts/pkgacct $S ;done
